f7c0c82f129bdc31522509eaf4e44fa9a59865f5
Lawrence Tsang <pikalaw@gmail.com>
2013-05-10 17:12:39 -0400

    Recursive version.

--- longest_palindrome_subsequence.py
+++ longest_palindrome_subsequence.py
@@ -28,6 +28,10 @@ def LongestPalindromeSubsequence(word):
           results[i][j] = Result(
               results[i][j-1].max_length, '', results[i][j-1])
 
+  return ConstructSolution(results)
+
+
+def ConstructSolution(results):
   # Reconstruct the solution.
   result = results[0][-1]
   solution = result.new_letter
@@ -39,6 +43,45 @@ def LongestPalindromeSubsequence(word):
     return solution + solution[::-1]
   else:
     return solution[:-1] + solution[-1] + solution[-2::-1]
+
+
+def RecursiveLongestPalindromeSubsequence(word):
+  # Meorized version.
+  results = [[None for _ in range(len(word)+1)] for _ in range(len(word)+1)]
+  _RecursiveLongestPalindromeSubsequence(word, 0, len(word), results)
+  return ConstructSolution(results)
+
+
+def _RecursiveLongestPalindromeSubsequence(word, i, j, results):
+  # Meorized case.
+  if results[i][j]:
+    return results[i][j]
+
+  # Base case.
+  if i == j:
+    results[i][j] = Result(0, '')
+    return results[i][j]
+  if j - i == 1:
+    results[i][j] = Result(1, word[i])
+    return results[i][j]
+
+  # Recursion case.
+  if word[i] == word[j-1]:
+    mid_result = _RecursiveLongestPalindromeSubsequence(
+        word, i+1, j-1, results)
+    results[i][j] = Result(mid_result.max_length + 2, word[i], mid_result)
+    return results[i][j]
+
+  left_result = _RecursiveLongestPalindromeSubsequence(
+      word, i, j-1, results)
+  right_result = _RecursiveLongestPalindromeSubsequence(
+      word, i+1, j, results)
+  if left_result.max_length > right_result.max_length:
+    results[i][j] = Result(left_result.max_length, '', left_result)
+    return results[i][j]
+  else:
+    results[i][j] = Result(right_result.max_length, '', right_result)
+    return results[i][j]
 
 
 import unittest
@@ -65,3 +108,23 @@ class TestLongestPalindromeSubsequence(unittest.TestCase):
     self.assertEqual(
         'aaa',
         LongestPalindromeSubsequence('aaa'))
+
+  def test_Recursive(self):
+    self.assertEqual(
+        'aba',
+        RecursiveLongestPalindromeSubsequence('aba'))
+    self.assertEqual(
+        'carac',
+        RecursiveLongestPalindromeSubsequence('character'))
+    self.assertEqual(
+        '',
+        RecursiveLongestPalindromeSubsequence(''))
+    self.assertEqual(
+        'a',
+        RecursiveLongestPalindromeSubsequence('a'))
+    self.assertEqual(
+        'aa',
+        RecursiveLongestPalindromeSubsequence('aa'))
+    self.assertEqual(
+        'aaa',
+        RecursiveLongestPalindromeSubsequence('aaa'))
