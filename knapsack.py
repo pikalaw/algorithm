6576f23d98cd182a7aee631ed16ae917556103b0
Lawrence Tsang <pikalaw@gmail.com>
2013-05-13 17:44:02 -0400

    continuous knapsack

--- knapsack.py
+++ knapsack.py
@@ -1,11 +1,13 @@
+import random
 
-def RecursiveKnapsack(values, weights, capacity):
+
+def ZeroOneKnapsack(values, weights, capacity):
   assert len(values) == len(weights)
   results = {}
-  return _RecursiveKnapsack(values, weights, capacity, len(values), results)
+  return _ZeroOneKnapsack(values, weights, capacity, len(values), results)
   
   
-def _RecursiveKnapsack(values, weights, capacity, n, results):
+def _ZeroOneKnapsack(values, weights, capacity, n, results):
   if capacity in results and n in results[capacity]:
     return results[capacity][n]
   
@@ -13,12 +15,12 @@ def _RecursiveKnapsack(values, weights, capacity, n, results):
     result = [], 0., 0.
   else:
     # Without the last item.
-    content_wo, value_wo, weight_wo = _RecursiveKnapsack(
+    content_wo, value_wo, weight_wo = _ZeroOneKnapsack(
         values, weights, capacity, n-1, results)
 
     # With the last item.
     if weights[n-1] <= capacity:
-      content_w, value_w, weight_w = _RecursiveKnapsack(
+      content_w, value_w, weight_w = _ZeroOneKnapsack(
           values, weights, capacity - weights[n-1], n-1, results)
       content_w += [n-1]
       value_w += values[n-1]
@@ -35,15 +37,94 @@ def _RecursiveKnapsack(values, weights, capacity, n, results):
   return result
 
 
+def ContinuousKnapsack(values, weights, capacity):
+  """Expected O(n)."""
+  assert len(values) == len(weights)
+  return _ContinuousKnapsack(values, weights, capacity, 0, len(values))
+
+
+def _ContinuousKnapsack(values, weights, capacity, start, end):
+  # Base case.
+  if capacity <= 0:
+    return []
+  if start >= end:
+    return []
+
+  # Recursion case.
+  pivot = random.randint(start, end - 1)
+  pivot, total_low_weight, total_high_weight = PartitionByDensity(
+      values, weights, start, pivot, end)
+
+  if total_high_weight > capacity:
+    return _ContinuousKnapsack(values, weights, capacity, pivot + 1, end)
+  else:
+    # Add all of the high density.
+    solution = [(i, weights[i]) for i in range(pivot + 1, end)]
+    remaining_capacity = capacity - total_high_weight
+
+    # Add as much the middle density as available.
+    if remaining_capacity > 0:
+      pivot_contributed_weight = min([remaining_capacity, weights[pivot]])
+      solution += [(pivot, pivot_contributed_weight)]
+      remaining_capacity -= pivot_contributed_weight
+
+    # Add as much the low density as available.
+    if remaining_capacity > 0:
+      solution += _ContinuousKnapsack(
+          values, weights, remaining_capacity, start, pivot)
+    return solution
+  
+
+def PartitionByDensity(values, weights, start, pivot, end):
+  pivot_density = Density(values, weights, pivot)
+  Swap(values, weights, pivot, end - 1)
+
+  low_weight, high_weight = 0., 0.
+  low = high = start
+  while high < end - 1:
+    density = Density(values, weights, high)
+    if density < pivot_density:
+      low_weight += weights[high]
+      Swap(values, weights, high, low)
+      low += 1
+    else:
+      high_weight += weights[high]
+    high += 1
+  Swap(values, weights, low, end - 1)
+
+  return low, low_weight, high_weight
+
+
+def Swap(values, weights, i, j):
+  temp_value = values[i]
+  temp_weight = weights[i]
+  values[i] = values[j]
+  weights[i] = weights[j]
+  values[j] = temp_value
+  weights[j] = temp_weight
+
+
+def Density(values, weights, i):
+  return float(values[i]) / float(weights[i])
+
+
 import unittest
 
 
 class TestKnapsack(unittest.TestCase):
 
-  def test_Example(self):
+  def test_ZeroOne(self):
     values = [60, 100, 120]
     weights = [10, 20, 30]
     capacity = 50
     self.assertEqual(
         ([1, 2], 220.0, 50.0),
-        RecursiveKnapsack(values, weights, capacity))
+        ZeroOneKnapsack(values, weights, capacity))
+
+  def test_Continuous(self):
+    values = [60, 100, 120]
+    weights = [10, 20, 30]
+    capacity = 50
+    self.assertEqual(
+        [(0, 20.), (1, 20.), (2, 10.)],
+        sorted(ContinuousKnapsack(values, weights, capacity)))
