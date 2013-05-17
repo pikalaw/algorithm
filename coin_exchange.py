6b463f4f35ccfd2aa1c459f6a1cd9a295b17a2fc
Lawrence Tsang <pikalaw@gmail.com>
2013-05-17 17:59:55 -0400

    Exchange coins iteratively.

--- coin_exchange.py
+++ coin_exchange.py
@@ -5,7 +5,7 @@ def RecursiveExchangeCoins(amount, coins):
   results = {}
   coins.sort()
   result = _RecursiveExchangeCoins(amount, coins, results)
-  return ComposeSolution(result)
+  return ComposeSolution(result, amount)
 
 
 class Result(object):
@@ -21,13 +21,13 @@ def _RecursiveExchangeCoins(amount, coins, results):
   if amount in results:
     return results[amount]
 
-  # Base case.
-  if amount < coins[0]:
+  # Base cases.
+  if len(coins) < 1 or amount < coins[0]:
     return Result(0, 0, None) 
 
   # Recursion case.
   min_coin_count = sys.maxint
-  min_result = Result(0, 0, None) 
+  min_result = None
   for coin in coins:
     if coin <= amount:
       remaining_result = _RecursiveExchangeCoins(amount - coin, coins, results)
@@ -41,7 +41,7 @@ def _RecursiveExchangeCoins(amount, coins, results):
 
 
 def IterativeExchangeCoins(amount, coins):
-  results = []
+  results = [None for _ in xrange(amount + 1)]
   results[0] = Result(0, 0, None)
   for subamount in xrange(amount + 1):
     min_coin_count = sys.maxint
@@ -54,17 +54,22 @@ def IterativeExchangeCoins(amount, coins):
           min_coin_count = coin_count
           min_result = Result(coin, coin_count, remaining_result)
     results[subamount] = min_result
-  return ComposeSolution(results[-1])
+  return ComposeSolution(results[-1], amount)
 
 
-def ComposeSolution(result):
+def ComposeSolution(result, amount):
   coins = []
+  total = 0
   while result:
     if result.coin > 0:
       coins.append(result.coin)
+      total += result.coin
     result = result.next_result
-  coins.sort()
-  return coins
+  if total != amount:
+    raise ValueError('Unable to exchange for {}'.format(amount))
+  else:
+    coins.sort()
+    return coins
 
 
 import unittest
@@ -73,15 +78,27 @@ import unittest
 class TestExchangeCoins(unittest.TestCase):
 
   def setUp(self):
+    self.longMessage = True
     self.tests = [
         ([1, 10, 15], 20, [10, 10]),
         ([1, 5, 10, 25], 99, [1, 1, 1, 1, 10, 10, 25, 25, 25]),
+        ([], 0, []),
+    ]
+    self.fail_tests = [
+        ([10], 11),
+        ([10], 1),
     ]
       
   def test_Recursive(self):
     for test in self.tests:
       self.assertEqual(test[2], RecursiveExchangeCoins(test[1], test[0]))
+    for fail_test in self.fail_tests:
+      with self.assertRaises(ValueError):
+        RecursiveExchangeCoins(fail_test[1], fail_test[0])
       
   def test_Iterative(self):
     for test in self.tests:
       self.assertEqual(test[2], IterativeExchangeCoins(test[1], test[0]))
+    for fail_test in self.fail_tests:
+      with self.assertRaises(ValueError):
+        IterativeExchangeCoins(fail_test[1], fail_test[0])
