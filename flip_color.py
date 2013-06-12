918159fd378793d099b698c2c97b635f70c19ab6
Lawrence Tsang <pikalaw@gmail.com>
2013-06-12 16:03:15 -0400

    simplified logic and naming

--- flip_color.py
+++ flip_color.py
@@ -8,8 +8,8 @@ def MinFlipColor(cards):
   Returns:
     String of 1 and 0 where 1 = flip 0 = no flip. Same length as `cards`.
   """
-  w = MinFlipWithEndColor(cards, 'W')
-  b = MinFlipWithEndColor(cards, 'B')
+  w = MinFlipWithStartColor(cards, 'W')
+  b = MinFlipWithStartColor(cards, 'B')
   return ''.join([str(x) for x in (w if sum(w) < sum(b) else b)])
 
 
@@ -17,15 +17,16 @@ def FlipColor(color):
   return 'W' if color == 'B' else 'B'
 
 
-def MinFlipWithEndColor(cards, last_color):
-  f = []
-  for i in xrange(len(cards) - 1, -1 , -1):
-    if cards[i] != last_color:
-      f.insert(0, 1)
+def MinFlipWithStartColor(cards, start_color):
+  flips = []
+  color = start_color
+  for card_color in cards:
+    if card_color != color:
+      flips.append(1)
     else:
-      f.insert(0, 0)
-    last_color = FlipColor(last_color)
-  return f
+      flips.append(0)
+    color = FlipColor(color)
+  return flips
 
 
 import unittest
@@ -37,8 +38,8 @@ class TestFlipColor(unittest.TestCase):
     self.assertEqual('', MinFlipColor(''))
     self.assertEqual('0', MinFlipColor('B'))
     self.assertEqual('0', MinFlipColor('W'))
-    self.assertEqual('01', MinFlipColor('WW'))
-    self.assertEqual('10', MinFlipColor('BB'))
+    self.assertEqual('10', MinFlipColor('WW'))
+    self.assertEqual('01', MinFlipColor('BB'))
     self.assertEqual('010', MinFlipColor('WWW'))
     self.assertEqual('010', MinFlipColor('BBB'))
     self.assertEqual('0100', MinFlipColor('BBBW'))
