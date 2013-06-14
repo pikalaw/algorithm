13ff3066ba18e509a7103da866151e8824f2cc49
Lawrence Tsang <pikalaw@gmail.com>
2013-06-14 13:11:28 -0400

    Implemented longest draw.

--- longest_draw.py
+++ longest_draw.py
@@ -1,41 +1,81 @@
 #!/usr/bin/env python
 
-import itertools
+import copy
 import math
 
 
-def ComputeDistance(p, q):
+def Distance(p, q):
   return math.hypot(p[0] - q[0], p[1] - q[1])
 
 
-def BuildDistances(ps):
-  num_p = len(ps)
-  ds = [[0 for _ in range(num_p)] for _ in range(num_p)]
-  for i, j in itertools.product(range(num_p), repeat=2):
-    ds[i][j] = ComputeDistance(ps[i], ps[j])
-  return ds
+def Reaches(node):
+  i, j = node
+  for di in [-2, -1, 0, 1, 2]:
+    for dj in [-2, -1, 0, 1, 2]:
+      if di % 2 == 0 and dj % 2 == 0:
+        continue
+      else:
+        fi = i + di
+        fj = j + dj
+        if not (0 <= fi <= 2 and 0 <= fj <= 2):
+          continue
+        else:
+          yield fi, fj
 
 
-def ComputePathLength(path, ds):
-  path_len = 0.
-  for i, j in itertools.product(path[:-1], path[1:]):
-    path_len += ds[i][j]
-  return path_len 
+def LongestDraw():
+  """Find the longest Security Pattern on Android.
+
+  The Android lock screen has 9 dots:
+
+      x  x  x
+      x  x  x
+      x  x  x
+
+  You draw the correct pattern to unlock the screen.
+
+  This function finds the longest pattern you can draw.
+  """
+  solution = {'path': [], 'length': 0}
+  for root in [(0, 0), (0, 1), (1, 1)]:
+    visited = {}
+    for i in range(3):
+      for j in range(3):
+        visited[(i, j)] = False
+    this_path = {'path': [], 'length': 0}
+    DFS(root, root, visited, solution, this_path)
+  return solution
 
 
-def main():
-  ps = [(x, y) for x, y in itertools.product(range(3), range(3))]
-  ds = BuildDistances(ps)
+def DFS(parent_node, node, visited, solution, this_path):
+  is_leaf = True
 
-  paths = []
-  for path in itertools.permutations(range(len(ps))):
-    paths.append((path, ComputePathLength(path, ds)))
-    #print path, ComputePathLength(path, ds)
+  visited[node] = True
+  segment_length = Distance(parent_node, node)
+  this_path['path'].append(node)
+  this_path['length'] += segment_length
 
-  paths.sort(key=lambda x: x[1])
+  for next_node in Reaches(node):
+    if not visited[next_node]:
+      is_leaf = False
+      DFS(node, next_node, visited, solution, this_path)
 
-  print paths[-5:]
+  if is_leaf:
+    if this_path['length'] > solution['length']:
+      solution['path'] = copy.deepcopy(this_path['path'])
+      solution['length'] = this_path['length']
+
+  visited[node] = False
+  this_path['path'].pop()
+  this_path['length'] -= segment_length
 
 
-if __name__ == '__main__':
-  main()
+import unittest
+
+
+class TestLongestDraw(unittest.TestCase):
+
+  def test_Example(self):
+    self.assertEqual(
+        [(0,1), (2,2), (1,0), (0,2), (2,1), (0,0), (1,2), (2,0), (1,1)],
+        LongestDraw()['path'])
