2bab36a44fbd585df6feffe1b73eb2d3bb3c7921
Lawrence Tsang <pikalaw@gmail.com>
2013-06-15 19:35:32 -0400

    Fixed missing leading code 0.

--- shortest_codebook.py
+++ shortest_codebook.py
@@ -13,7 +13,8 @@ def ShortestCodebook(digits):
   num_codes = 10**digits
   root = 0
   visited = [False for _ in xrange(num_codes)]
-  return ''.join([str(x) for x in DFS(root, visited, 0, num_codes)])
+  codebook = [0] * (digits-1) + DFS(root, visited, 0, num_codes)
+  return ''.join([str(x) for x in codebook])
 
 
 def DFS(code, visited, num_codes_reached, num_codes):
@@ -43,10 +44,20 @@ import unittest
 
 class TestShortestCodebook(unittest.TestCase):
 
+  def VerifyAllCodes(self, codebook, digits):
+    visited = [False for _ in xrange(10**digits)]
+    i = 0
+    while i + digits <= len(codebook):
+      visited[int(codebook[i:i+digits])] = True
+      i += 1
+    for i, v in enumerate(visited):
+      self.assertTrue(v, msg='{} is not in the codebook'.format(i))
+
   def test_Example(self):
-    for digits in range(5):
+    for digits in range(1, 5):
       sys.setrecursionlimit(10**digits + 1000)
       codebook = ShortestCodebook(digits)
       print '{} digits requires {} length codebook (first 100): {}'.format(
           digits, len(codebook), codebook[:100])
-      self.assertEqual(10**digits, len(codebook))
+      self.assertEqual(10**digits + digits - 1, len(codebook))
+      self.VerifyAllCodes(codebook, digits)
