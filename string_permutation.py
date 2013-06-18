cb3e1ec439cdcdf564f70352368b214b19c09b97
Lawrence Tsang <pikalaw@gmail.com>
2013-06-18 18:33:24 -0400

    More pythonic version of permutation.

--- string_permutation.py
+++ string_permutation.py
@@ -1,19 +1,18 @@
 
-def StringPermutation(string, visit):
-  _StringPermutation(list(string), visit, 0)
+def StringPermutation(s):
+  for p in Permutation(list(s)):
+    yield ''.join(p)
 
 
-def _StringPermutation(string, visit, i):
-  if i == len(string):
-    visit(''.join(string))
+def Permutation(s):
+  if len(s) <= 1:
+    yield s
   else:
-    prior_character = None
-    for j in xrange(i, len(string)):
-      if prior_character != string[j]:
-        Swap(string, i, j)
-        _StringPermutation(string, visit, i + 1)
-        Swap(string, i, j)
-        prior_character = string[j]
+    for i in xrange(len(s)):
+      Swap(s, 0, i)
+      for p in Permutation(s[1:]):
+        yield [s[0]] + p
+      Swap(s, 0, i)
     
 
 def Swap(a, i, j):
@@ -22,37 +21,17 @@ def Swap(a, i, j):
   a[j] = t
 
 
-import mox
 import unittest
 
 
 class TestStringPermutation(unittest.TestCase):
 
   def test_Unique(self):
-    test_string = 'abc'
-
-    m = mox.Mox()
-    mock_visit = m.CreateMockAnything()
-    mock_visit.__call__('abc')
-    mock_visit.__call__('acb')
-    mock_visit.__call__('bac')
-    mock_visit.__call__('bca')
-    mock_visit.__call__('cba')
-    mock_visit.__call__('cab')
-    m.ReplayAll()
-
-    StringPermutation(test_string, mock_visit)
-    m.VerifyAll()
+    actual = list(StringPermutation('abc'))
+    expected = ['abc', 'acb', 'bac', 'bca', 'cba', 'cab']
+    self.assertEqual(actual, expected)
 
   def test_Duplicate(self):
-    test_string = 'aac'
-
-    m = mox.Mox()
-    mock_visit = m.CreateMockAnything()
-    mock_visit.__call__('aac')
-    mock_visit.__call__('aca')
-    mock_visit.__call__('caa')
-    m.ReplayAll()
-
-    StringPermutation(test_string, mock_visit)
-    m.VerifyAll()
+    actual = list(StringPermutation('abb'))
+    expected = ['abb', 'abb', 'bab', 'bba', 'bba', 'bab'] 
+    self.assertEqual(actual, expected)
